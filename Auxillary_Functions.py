0e18b1ca7e1a10560ba1e0f6acdbbe42a29142a4
Andrew Martinez <andrew.j.martinez@student.ie.edu>
2019-04-07 00:59:44 +0200

    Update Auxillary_Functions.py
    Remove unnecessary function

--- Auxillary_Functions.py
+++ Auxillary_Functions.py
@@ -1,14 +1,5 @@
 import numpy as np
 import pandas as pd
-import yaml
-
-#Imports YML file give input path
-def read_yml(input_path):
-    with open(input_path, 'r') as stream:
-        try:
-            print(yaml.load(stream))
-        except yaml.YAMLError as exc:
-            print(exc)
 
 #Imports CSV file give input path
 def read_data(input_path):
